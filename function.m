f9d87298f46e4654a835d198b71c7b77ce3195fc
ajeet <mr.akash369singh@gmail.com>
2026-08-15 01:11:24 +0530

    cmmit

---
+++ function.m
@@ -0,0 +1,8 @@
+a = 2;
+b = a*a;
+c = a*a*a;
+d = sqrt(a);
+fprintf('The value of a is: %d\n', a);
+fprintf('The value of b is: %d\n', b);
+fprintf('The value of c is: %d\n', c);
+fprintf('The value of d is: %f\n', d);
---
+++ test.m
@@ -0,0 +1,9 @@
+clc;
+clear;
+
+a = 10;
+b = 20;
+
+c = a + b;
+
+disp(c);
