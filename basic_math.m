d2cddfd0ff0a13bb120fc0018f49ee52308f1f41
ajeet <mr.akash369singh@gmail.com>
2026-09-01 22:58:20 +0530

    Create basic_math.m

---
+++ basic_math.m
@@ -0,0 +1,24 @@
+a = [1,0,2;3,4,5];
+a2 = [3,4,5;6,7,8];
+a
+
+b =  2.*a;           %. mins element by element and * mins multiplication
+b
+
+c =  2.+a;           %. mins element by element and * mins addition
+c
+
+d =  2.-a;           %. mins element by element and * mins subtraction
+d
+
+b =  2.*a;           %. mins element by element and * mins multiplication
+b
+
+e =  2.^a;           %. mins element by element and * mins power
+e
+
+
+
+B =  a2.*a;           %. mins element by element and * mins multiplication
+B
+B2 = a*a2;           %. mins  multiplication
