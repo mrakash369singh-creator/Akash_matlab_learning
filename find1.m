f41b46f1fa7bde64ce5ac008fa14abd558e291d0
ajeet <mr.akash369singh@gmail.com>
2026-09-03 14:20:09 +0530

    Create find1.m

---
+++ find1.m
@@ -0,0 +1,12 @@
+% How to Identifying Non Zero Element in a Matrix 
+% find(X)  liner index
+%  find(x,n)       x----> find nonzeros element in matrix
+%  find(~x,n)       x----> find zeros element in matrix
+
+X  = [1,0,2;0,1,3;0,0,5];
+X
+
+
+n = find(X)
+
+n1 = finfd(~X)
