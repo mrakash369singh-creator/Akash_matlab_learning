03323e5789dce051081ebfd673266d685121b616
ajeet <mr.akash369singh@gmail.com>
2026-09-08 22:04:08 +0530

    Create predefine.m

---
+++ predefine.m
@@ -0,0 +1,25 @@
+% Predefined Keyword for Mathematical Operations in Matlab
+
+% dis
+%clc
+%close all
+%clear all
+%input
+%sin
+%cos
+%asin
+%acos
+%pi
+%exp
+%log
+%power
+%sqrt
+%zeros
+%ones
+%magic
+%ceil
+%floor
+%
+%
+%
+%
