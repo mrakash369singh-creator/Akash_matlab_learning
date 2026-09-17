eca0aa34eadb03503ea161ce36433f95656ca588
ajeet <mr.akash369singh@gmail.com>
2026-09-18 01:38:11 +0530

    Create sorting_element.m

---
+++ sorting_element.m
@@ -0,0 +1,12 @@
+a  = [1,2,3,-3,-4,0,5,-2,4,5,];
+sort(a)       % accending order
+
+sort(a,'decend')      % decending order
+
+
+
+
+sort(a,2)         %row wise sorting
+sort(a,2)          %collum wise sorting 
+
+B = sort(a,2,'descend')
